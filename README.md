6623b9576a61d3c79b6e005648e7af527b9363af
Brian <bjk69@pitt.edu>
2016-02-01 00:15:10 -0500

    Added more comments and prepared for submission.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CS0401-Assignment-1
 Repository for the first assignment of CS0401 MW Spring 2016
 
-1/24: The program currently works as described in the rubric with the addition of the bookworm card.
+1/31: The program currently works as described in the rubric with the addition of the bookworm card.
 
-To do: Add the book personalization feature and clean up code - add comments at least
+To do: Merge with master branch and submit
--- assignment1.java
+++ assignment1.java
@@ -1,5 +1,7 @@
 /*
 Assignment 1 - bookstore
+Brian Knotten
+
 Items: 
 1. Books $5.00 each
 2. Bookmarks $1.00 each or a pack of six for $5.00 - always give a pack when possible 
@@ -15,26 +17,41 @@ public class assignment1{
 
 	public static void main(String[] args){
 
+		// Create a new scanner for reading input and
+		// declare the variables that will store the amount of each
+		// item in a customer's cart, as well as the booleans for 
+		// both the 3rd customer and bookworm card discounts.
+		// The final variable sets the initial customer's number to 1.
+
 		Scanner sc = new Scanner(System.in);
-	
-		System.out.print("Are there more customers in line? (1 = Yes, 2 = No): ");
-		
-		int custInt = sc.nextInt(); 
-		int custNum = 1;
-		boolean discount = false;
-		boolean bookDiscount = false;
+			
 		int cartBooks = 0;
 		int cartMarks = 0;
 		int cartPaint = 0;
+		boolean discount = false;
+		boolean bookDiscount = false;
+		int custNum = 1;
+
+		// The initial prompt. If the user enters 1 the program enters a while loop
+		// of shopping options. This prompt follows every "checkout" and will continue
+		// 2 is entered. If 2 is entered initially the program ends immediately.		
+
+		System.out.print("Are there more customers in line? (1 = Yes, 2 = No): ");
+		int custInt = sc.nextInt();
 
 		while (custInt == 1){
 			
+			// If the customer's number is divisible by 3 the 10% discount
+			// is set to true - giving a discount to every 3rd customer			
+
 			if (custNum % 3 == 0){
 				discount = true;
 			}
 			else{
 				discount = false;
 			}
+
+			// Every time the program loops the user is presented with the 5 options
 
 			System.out.println("1 - Buy Books - $5.00 each ");
 			System.out.println("2 - Buy Bookmarks - $1.00 each, $5.00 for a six-pack ");
@@ -44,6 +61,11 @@ public class assignment1{
 			System.out.print("Please enter a valid option (1 through 5): ");
 			int optInt = sc.nextInt();
 
+			// Options 1-3 prompt the user to enter the number (0 or greater) of items
+			// they want to purchase after displaying the number already in their cart.
+			// The number entered is stored in an intermediate variable before being added
+			// to the cart variable.
+		
 			if (optInt == 1){
 				System.out.println("Currently in cart: " + cartBooks + " books");
 				System.out.print("How many books do you want to buy? ");
@@ -78,17 +100,27 @@ public class assignment1{
 				cartPaint += buyPaint;
 			}
 			
+			// Option 4 displays the number of each item in the user's cart.
+
 			else if (optInt == 4){
 				System.out.println("Currently in cart:");
 				System.out.println("Books: " + cartBooks);
 				System.out.println("Bookmarks: " + cartMarks);
 				System.out.println("Paintings of books: " + cartPaint);
 			}
+
+			// Option 5 begins the checkout process
 			
 			else if (optInt == 5){
+
+				// The process begins with the declaration of necessary intermediate variables 
+				// which are all set to 0.
 								
 				double booksCost, marksCost, packsCost, paintCost, subsubtotal, discountTotal, subtotal;
 				booksCost = marksCost = packsCost = paintCost = subsubtotal = discountTotal = subtotal = 0;
+
+				// If the user has any books in their cart they are asked if they have a bookworm card
+				// and the bookDiscount is set appropriately.
 
 				if (cartBooks > 0){
 					System.out.print("Do you have a Bookworm Card? (1 = yes 2 = no) ");
@@ -103,6 +135,9 @@ public class assignment1{
 				}
 
 				System.out.println("----------------------------------------------------");
+
+				// The 3rd customer discount variable is checked and the appropriate message is displayed
+
 				if (discount == true){
 					System.out.println("You won a 10% discount!");
 				}
@@ -110,13 +145,21 @@ public class assignment1{
 				else {
 					System.out.println("You did not get a discount! Better luck next time!");
 				}
-					
+				
+				// The cart variables are cycled through and for each greater than 0 the 
+				// cost is calculated and stored in the intermediate double variable
+				// before being displayed.
+
 				if (cartBooks > 0){
 					booksCost = cartBooks * 5.0;
 					System.out.printf("\t" + cartBooks + " Book(s):\t\t$%.2f\n", booksCost);
 				}
 				
 				if (cartMarks > 0){
+
+					// If applicable, the number of bookmarks is split into packs and the cost is 
+					// calculated separately for the number of packs and the number of remaining bookmarks
+
 					if (cartMarks < 6){
 						marksCost = (double)cartMarks;
 						System.out.printf("\t" + cartMarks + " Single Bookmark(s):\t$%.2f\n", marksCost);
@@ -136,6 +179,9 @@ public class assignment1{
 					paintCost = cartPaint * 100.0;
 					System.out.printf("\t" + cartPaint + " Painting(s):\t\t$%.2f\n", paintCost);
 				}
+
+				// A subtotal is calculated before any and all discounts are applied and 
+				// discount messages are displayed accordingly
 
 				if (discount == true){
 					subsubtotal = booksCost + packsCost + marksCost + paintCost;
@@ -162,7 +208,10 @@ public class assignment1{
 						System.out.printf("Bookworm Discount! Saved: \t-$%.2f\n", discountTotal);
 					}
 				}
-					
+				
+				// The subtotal, tax added, and total are all displayed before prompting 
+				// the user to enter an appropriate payment amount.
+
 				System.out.printf("\nSubtotal:\t\t\t$%.2f\n", subtotal);
 
 				double tax = subtotal * 0.07;
@@ -177,24 +226,40 @@ public class assignment1{
 					System.out.print("Not enough money, please re-enter: ");
 					payment = sc.nextDouble();
 				}
+
+				// Change is calculated and displayed with a thank you message.
+
 				double change = payment - total;
 				System.out.printf("\nYour change is: $%.2f\n", change);
 				System.out.println("Thanks for shopping!\n");
 				
+				// The cart and discount variables are set to false and the customer
+				// number is incremented (The bookworm discount is manually set with
+				// every customer so it does not need to be reset).
+
 				cartBooks = cartMarks = cartPaint = 0;
 				discount = false;
 				custNum ++;
 			
+				// The user presented the initial prompt again - 1 continues the while
+				// loop and 2 ends the program.
+
 				System.out.print("More customers in line? (1 = Yes, 2 = No): ");
 				custInt = sc.nextInt();
 			
 			}
 			
+			// This is the response to invalid inputs for the while loop (anything besides 1-5).
+			// The loop continues immediately after this message is displayed.
+
 			else {
 				System.out.println("Please enter a valid option (1 through 5) ");
 			}
 		
 		}
+
+	// The message displayed at the end of the program.
+
 	System.out.println("No more customers! Closing!");
 	}
 }
