c4139ea9f94350ea5b6657d8925e8f46b9aa5092
Brian <bjk69@pitt.edu>
2016-03-24 22:34:50 -0400

    Added Assignment3

---
+++ Assignment3/Quiz.java
@@ -0,0 +1,14 @@
+/*
+Assignment 3 - Quiz
+Brian Knotten
+*/
+
+public class Quiz{
+    public static void main(String[] args){
+	if (args.length > 0){
+	    System.out.println("First arg is: " + args[0]);
+	}else{
+	    System.out.println("No args given!");
+	}
+    }
+}	    
